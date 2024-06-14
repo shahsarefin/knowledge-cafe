93634b0231d9988451dcc970d200c97aa9885992
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-06-14 11:46:47 -0500

    Displayed Blog Datas and Hashtags

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,9 +2,36 @@ import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
     console.log(blog);
+    const {title, cover, reading_time, author, author_img, posted_date, hashtags} = blog;
     return (
         <div>
-            
+
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+
+            <div className='flex justify-between'>
+
+                <div className='flex'>
+                    <img className="w-14" src={author_img} alt="" />
+
+                    <div>
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+
+                </div>
+
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+
+            <h2 className="text-4xl">{title}</h2>
+
+            <p>
+                {
+                    hashtags.map((hash,idx)=> <span key={hash.idx}> <a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Blog from '../Blog/Blog';
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -6,13 +7,19 @@ const Blogs = () => {
     useEffect(() => {
         fetch('/blogs.json')
         .then(res => res.json())
-        .then(data => console.log(data))
-        .then(data => setBlogs(data))
-    }, []);
+        .then(data => {
+            console.log(data);
+            setBlogs(data);
+        })
+     }, []);
 
     return (
         <div className='md:w-2/3'>
             <h2>Blogs Section</h2>
+
+            {
+                blogs.map(blog => <Blog blog={blog} key={blog.id}></Blog>)
+            }
         </div>
     );
 };
