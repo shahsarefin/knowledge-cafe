ade36e8484a92feafc4ee6743bd97d3ab69d6b5f
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-06-14 11:19:05 -0500

    Header added, Blog and Bookmark layout created for medium device, Proptypes added for single Blog component

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -11,8 +11,8 @@ const Blogs = () => {
     }, []);
 
     return (
-        <div>
-            
+        <div className='md:w-2/3'>
+            <h2>Blogs Section</h2>
         </div>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import profilePic from '../../assets/images/profile.png';
 const Header = () => {
     return (
-        <div>
+        <div className="flex justify-between items-center p-4 mx-4">
             <h1 className="text-3xl font-bold">Knowledge Cafe</h1>
+            <img src={profilePic} alt="" />
         </div>
     );
 };
